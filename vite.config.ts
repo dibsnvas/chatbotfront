cd9f428de4069ca96bac451d32a2e84800b755f3
dibsnvas <d_beisenbayeva@kbtu.kz>
2024-07-25 20:41:37 +0500

    fixedd

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    outDir: 'dist', 
-    assetsDir: '.', 
-    rollupOptions: {
-      input: {
-        main: './src/main.tsx', 
-      },
-    },
-  },
+    outDir: 'dist'
+  }
 });
